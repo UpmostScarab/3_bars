78e5c1479250f023930d16910eca2bf479d3490b
aumnov <sasha@umnov.org>
2016-09-15 21:25:33 +0300

    Написаны функции

--- bars.py
+++ bars.py
@@ -2,20 +2,26 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file:
+        bars = json.loads(file.read())
+    return bars
 
 
 def get_biggest_bar(data):
-    pass
+    return max(bars, key = lambda x: x['Cells']['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    pass
-
+    return min(bars, key = lambda x: x['Cells']['SeatsCount'])
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    return min(bars, key = lambda x: (x['Cells']['geoData']['coordinates'][0] - longitude)**2  + (x['Cells']['geoData']['coordinates'][1] - latitude)**2)
 
 
 if __name__ == '__main__':
-    pass
+    bars = load_data('bars.json')
+    print(get_biggest_bar(bars))
+    print(get_smallest_bar(bars))
+    lat = float(input('Введите широту: '))
+    lon = float(input('Введите долготу: '))
+    print(get_closest_bar(bars, lon, lat))
