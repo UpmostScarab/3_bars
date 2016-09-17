160b9c830d068d027179ece0bf2f88b2920b309b
UpmostScarab <fuzzykiller2@gmail.com>
2016-09-17 12:37:25 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,5 +1,8 @@
 import json
 
+def distance(x1,y1,x2,y2):
+    return (x1 - x2)**2 + (y1 - y2)**2
+
 
 def load_data(filepath):
     with open(filepath) as file:
@@ -15,13 +18,14 @@ def get_smallest_bar(data):
     return min(bars, key = lambda x: x['Cells']['SeatsCount'])
 
 def get_closest_bar(data, longitude, latitude):
-    return min(bars, key = lambda x: (x['Cells']['geoData']['coordinates'][0] - longitude)**2  + (x['Cells']['geoData']['coordinates'][1] - latitude)**2)
+    return min(bars, key = lambda x: \
+        distance(*x['Cells']['geoData']['coordinates'], longitude, latitude))
 
 
 if __name__ == '__main__':
     bars = load_data('bars.json')
-    print(get_biggest_bar(bars))
-    print(get_smallest_bar(bars))
+    print('Самый большой бар:',get_biggest_bar(bars))
+    print('Самый маленький бар: ',get_smallest_bar(bars))
     lat = float(input('Введите широту: '))
     lon = float(input('Введите долготу: '))
-    print(get_closest_bar(bars, lon, lat))
+    print("Самый близкий бар",get_closest_bar(bars, lon, lat))
