a0760e0fc0430e0dcdd1911259e867b28131ffc5
aumnov <sasha@umnov.org>
2016-09-17 13:02:41 +0300

    Fix

--- bars.py
+++ bars.py
@@ -1,12 +1,13 @@
 import json
+from functools import partial
 
 def distance(x1,y1,x2,y2):
     return (x1 - x2)**2 + (y1 - y2)**2
 
 
 def load_data(filepath):
-    with open(filepath) as file:
-        bars = json.loads(file.read())
+    with open(filepath) as input_file:
+        bars = json.loads(input_file.read())
     return bars
 
 
@@ -18,8 +19,8 @@ def get_smallest_bar(data):
     return min(bars, key = lambda x: x['Cells']['SeatsCount'])
 
 def get_closest_bar(data, longitude, latitude):
-    return min(bars, key = lambda x: \
-        distance(*x['Cells']['geoData']['coordinates'], longitude, latitude))
+    partial_distance = partial(distance,longitude, latitude)
+    return min(bars, key = lambda x: partial_distance(*x['Cells']['geoData']['coordinates']))
 
 
 if __name__ == '__main__':
